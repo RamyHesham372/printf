bc5cdf927115da497c70141e8ba8c9a256a1e515
Kareem <kareem@gmail.com>
2023-07-21 22:55:50 +0300

    pushing printf header file

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+/**
+ * struct print_f - struct that
+ *
+ * description -
+ */
+typedef struct print_f
+{
+
+} _printf;
+
+
+
+/*prototypes*/
+int _printf(const char *format, ...);
+
+
+
+
+
+
+#endif
